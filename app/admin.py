2e87f6c60a02f3eb292ba874648f0c2a0d995da6
“shawazi” <shawazisonfire@gmail.com>
2023-05-10 19:01:57 -0400

    updated

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Song, Album, Artist, Genre
 
 # Register your models here.
+
+admin.site.register([Song, Album, Artist, Genre])
--- app/models.py
+++ app/models.py
@@ -5,17 +5,30 @@ from django.db import models
 class Genre(models.Model):
     name = models.CharField(max_length=50)
     
+    def __str__(self):
+        return self.name
+    
 class Album(models.Model):
     title = models.CharField(max_length=50)
     genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True)
+    # on_delete=models.PROTECT <--- prevents deletion if that genre still contains albums
     release_date = models.DateField()
+    
+    def __str__(self):
+        return self.title
 
 class Artist(models.Model):
     name = models.CharField(max_length=50)
     albums = models.ManyToManyField(Album)
+    
+    def __str__(self):
+        return self.name
     
 class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     length = models.BigIntegerField()
     track_number = models.BigIntegerField()
+    
+    def __str__(self):
+        return self.title + " from album: " + self.album
